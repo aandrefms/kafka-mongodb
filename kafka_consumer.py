75709cc72acbe2a9ec1200024b77df2d61b56014
Andre Felippe <andre.felippemsp@gmail.com>
2021-01-15 13:57:28 -0300

    15/01

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,6 +1,6 @@
 from kafka.consumer import KafkaConsumer
-# import config_db
-# import ast
+import config_db
+import ast
 
 kafka_boostrap_servers = '127.0.0.1:9092'
 kafka_topic_name = 'temperaturas'
@@ -8,6 +8,6 @@ kafka_topic_name = 'temperaturas'
 consumer = KafkaConsumer(kafka_topic_name, bootstrap_servers=kafka_boostrap_servers,
                          auto_offset_reset='earliest', enable_auto_commit=False)  # earliest
 for message in consumer:
-    # item = message.value.decode('utf-8')
-    '''config_db.inserir_database(ast.literal_eval(item))'''
+    item = message.value.decode('utf-8')
+    config_db.inserir_database(ast.literal_eval(item))
     print(message.value.decode('utf-8'))
