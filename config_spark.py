ed29b271d3438a81c4ad203a155bbc10385af319
Andre Felippe <andre.felippemsp@gmail.com>
2021-01-15 13:53:49 -0300

    15/01

--- config_spark.py
+++ config_spark.py
@@ -19,17 +19,17 @@ lines = spark \
     .option("startingOffsets", "latest") \
     .load()
 lines.printSchema()
-# Split the lines into words
+
 words = lines.select(
    explode(
        split(lines.value, " ")
    ).alias("word")
 )
 
-# Generate running word count
+
 wordCounts = words.groupBy("word").count()
 
-# Start running the query that prints the running counts to the console
+
 query = wordCounts \
     .writeStream \
     .outputMode("complete") \
